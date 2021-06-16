8e03d16ea1c41c4b4b38e2be5987e26c0685f8f0
ChrisB <christopher.brownlie@education.gov.uk>
2021-06-16 12:24:03 +0100

    fix: reference shinydashboard in UI creation

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -7,115 +7,115 @@ sass::sass(
 )
 
 # Header definition
-header <- dashboardHeader(title = "R Shiny")
+header <- shinydashboard::dashboardHeader(title = "R Shiny")
 
 # Sidebar definition
-sidebar <- dashboardSidebar(
-  sidebarMenu(
+sidebar <- shinydashboard::dashboardSidebar(
+  shinydashboard::sidebarMenu(
 
-    menuItem(
+    shinydashboard::menuItem(
       "About this app",
       tabName = "0_about_this_app"
     ),
 
     # Section 1 'How Shiny Works'
-    menuItem(
+    shinydashboard::menuItem(
       "1 - How shiny works",
       startExpanded = TRUE,
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "1.1 - What is shiny, actually?",
         tabName = "1_1_what_is_shiny"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "1.2 - The reactive graph",
         tabName = "1_2_reactive_graph"
         ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "1.3 - Controlling reactivity",
         tabName = "1_3_controlling_reactivity"
         ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "1.4 - reactive() vs observe()",
         tabName = "1_4_reactive_vs_observe"
         ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "1.5 - Structuring an app",
         tabName = "1_5_structuring_an_app"
         ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "1.6 - Shiny modules",
         tabName = "1_6_shiny_modules"
         )
       ),
 
     # Section 2 'Making it pretty'
-    menuItem(
+    shinydashboard::menuItem(
       "2 - Making it pretty",
       startExpanded = TRUE,
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "2.1 - Improving UI with CSS",
         tabName = "2_1_improving_ui_with_css"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "2.2 - Improving UX with JS",
         tabName = "2_2_improving_ux_with_js"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "2.3 - The attali-verse",
         tabName = "2_3_the_attali_verse"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "2.4 - Extending shiny",
         tabName = "2_4_extending_shiny"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "2.5 - Custom shiny inputs",
         tabName = "2_5_custom_shiny_inputs"
       )
     ),
 
     # Section 3 'Improvements & Plots'
-    menuItem(
+    shinydashboard::menuItem(
       "3 - Improvements & Plots",
       startExpanded = TRUE,
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "3.1 - The session object",
         tabName = "3_1_the_session_object"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "3.2 - Tracking usage",
         tabName = "3_2_tracking_usage"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "3.3 - Advanced viz 1 (plotly)",
         tabName = "3_3_advanced_vis_1_plotly"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "3.4 - Advanced viz 2 (r2d3)",
         tabName = "3_4_advanced_vis_2_r2d3"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "3.5 - Error handling & debugging",
         tabName = "3_5_error_handling_and_debugging"
       ),
 
-      menuSubItem(
+      shinydashboard::menuSubItem(
         "3.6 - Optimisation",
         tabName = "3_6_optimisation"
       )
@@ -125,7 +125,7 @@ sidebar <- dashboardSidebar(
 )
 
 # Body definition
-body <- dashboardBody(
+body <- shinydashboard::dashboardBody(
 
   tags$head(
 
@@ -141,100 +141,100 @@ body <- dashboardBody(
   tags$script(src = "initiate_clipboard.js"),
 
   # Call each module UI which will contain the definition for each tab
-  tabItems(
+  shinydashboard::tabItems(
 
 
     # Section 0 - the landing page
-    tabItem(
+    shinydashboard::tabItem(
       "0_about_this_app",
       tab_info_ui("info")
     ),
 
     # Section 1 - How Shiny Works
-    tabItem(
+    shinydashboard::tabItem(
       "1_1_what_is_shiny",
       tab_whatIsShiny_ui("whatIsShiny")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "1_2_reactive_graph",
       tab_reactiveGraph_ui("reactiveGraph")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "1_3_controlling_reactivity",
       tab_controllingReactivity_ui("controllingReactivity")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "1_4_reactive_vs_observe",
       tab_reactiveObjects_ui("reactiveObjects")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "1_5_structuring_an_app",
       tab_appStructure_ui("appStructure")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "1_6_shiny_modules",
       tab_shinyModules_ui("shinyModules")
     ),
 
 
     # Section 2 - Making it pretty
-    tabItem(
+    shinydashboard::tabItem(
       "2_1_improving_ui_with_css",
       tab_usingCSS_ui("usingCSS")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "2_2_improving_ux_with_js",
       tab_usingJS_ui("usingJS")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "2_3_the_attali_verse",
       tab_attaliverse_ui("attaliverse")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "2_4_extending_shiny",
       tab_extendingShiny_ui("extendingShiny")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "2_5_custom_shiny_inputs",
       tab_customInputs_ui("customInputs")
     ),
 
     # Section 3 - Improvements & Analytics
-    tabItem(
+    shinydashboard::tabItem(
       "3_1_the_session_object",
       tab_sessionObject_ui("sessionObject")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "3_2_tracking_usage",
       tab_trackingUsage_ui("trackingUsage")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "3_3_advanced_vis_1_plotly",
       tab_visualisationPlotly_ui("visualisationPlotly")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "3_4_advanced_vis_2_r2d3",
       tab_visualisationR2D3_ui("visualisationR2D3")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "3_5_error_handling_and_debugging",
       tab_errorHandling_ui("errorHandling")
     ),
 
-    tabItem(
+    shinydashboard::tabItem(
       "3_6_optimisation",
       tab_optimisation_ui("optimisation")
     )
